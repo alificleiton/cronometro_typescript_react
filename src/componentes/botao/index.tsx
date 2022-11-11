82de62f0eb3c3520b2a6442f13d16cd948c1dd50
unknown <alificleiton123@hotmail.com>
2022-11-11 14:23:09 -0300

    Adicionando novos arquivos

---
+++ src/componentes/botao/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import style from './style.module.scss';
+
+class Button extends React.Component <{ 
+  children: any 
+  type?: "button" | "submit" | "reset" | undefined, onClick?: () => void
+}> {
+  render() {
+    const { type = "button", onClick} = this.props;
+    return (
+      <button onClick={onClick} type={type} className={style.botao}>
+        {this.props.children}
+      </button>
+    );
+  }
+
+}
+
+export default Button;
+
+
